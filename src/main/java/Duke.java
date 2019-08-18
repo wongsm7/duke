1ece433c057a2e7c90e1ff64271d4c464319a2ea
wongsm7 <wongsm7@hotmail.com>
2019-08-19 00:07:47 +0800

    Completed level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,18 +9,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
+        List<String> listOfStrings = new ArrayList<>();
         while(sc.hasNext()) {
             String temp = sc.nextLine();
             if(temp.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+            else if(temp.equals("list")) {
+                int count = 1;
+                for(String s : listOfStrings) {
+                    System.out.println(count + ". " + s);
+                    count++;
+                }
+            }
             else {
-                System.out.println(temp);
+                System.out.println("added: " + temp);
+                listOfStrings.add(temp);
             }
         }
     }
