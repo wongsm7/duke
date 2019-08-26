129fa34afda40f9b269327eb5fc176b93f6a2a1b
wongsm7 <wongsm7@hotmail.com>
2019-08-26 09:57:16 +0800

    Merge Level-7

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,23 @@
-import java.util.Arrays;
+import java.time.format.DateTimeFormatter;
 
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String type;
+    protected final DateTimeFormatter OUT_DTF = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public Task(String description, String done){
+        this.description = description;
+        if(done.equals("1")) {
+            this.isDone = true;
+        } else {
+            this.isDone = false;
+        }
     }
 
     public String getStatusIcon() {
@@ -15,6 +26,23 @@ public class Task {
 
     public void setDone() {
         this.isDone = true;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public String getIsDone(){
+        if(isDone){
+            return "1";
+        }
+        else{
+            return "0";
+        }
+    }
+
+    public String getDescription(){
+        return this.description;
     }
 
     @Override
