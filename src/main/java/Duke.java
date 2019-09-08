fe1de8ecf1c71e6e3246e38f8f76ed032dedc299
Wong Shun Min <wongsm7@hotmail.com>
2019-09-08 11:49:12 +0800

    Remove unused imports

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,3 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-
 import java.io.IOException;
 
 /**
