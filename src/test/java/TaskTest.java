bc0a1f1480b7859cdb1aee77eb6e5231f141b719
Wong Shun Min <wongsm7@hotmail.com>
2019-09-15 16:16:17 +0800

    Update test

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -39,9 +39,9 @@ public class TaskTest {
     public void getTypeTest() {
         Todo t  = new Todo("buy book");
         assertEquals("T", t.getType());
-        Event e = new Event("buy book", "bookstore");
-        assertEquals("E", e.getType());
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+        Event e = new Event("buy book", LocalDateTime.parse("12/09/2019 1800", dtf));
+        assertEquals("E", e.getType());
         Deadline d = new Deadline("buy book", LocalDateTime.parse("12/09/2019 1800", dtf));
         assertEquals("D", d.getType());
     }
